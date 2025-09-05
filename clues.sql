6ddddf668df11bf6372ceb3ffdb37078a89a294e
xiokevc <xiokevc@gmail.com>
2025-09-05 10:40:29 -0400

    complete clue#2

--- clues.sql
+++ clues.sql
@@ -2,16 +2,26 @@
  
 -- Write SQL query here
 
-SELECT name
-FROM countries
-WHERE region = 'Southern Europe'
-ORDER BY population ASC
+SELECT Code, Name -- use "code" to avoid hardcoding names
+FROM country
+WHERE Region = 'Southern Europe'
+ORDER BY Population ASC
 LIMIT 1;
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
 
 -- Write SQL query here
 
+SELECT Language
+FROM countrylanguage
+WHERE IsOfficial = 'T'
+  AND CountryCode = (
+    SELECT Code
+    FROM country
+    WHERE Region = 'Southern Europe'
+    ORDER BY Population ASC
+    LIMIT 1
+  );
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
 
