d355ef5ebd4a4f76d11a7275cdb922bd099a34a1
xiokevc <xiokevc@gmail.com>
2025-09-07 17:07:47 -0400

    compelte clue#5

--- clues.sql
+++ clues.sql
@@ -103,6 +103,12 @@ LIMIT 1;
 
 -- Write SQL query here
 
+SELECT ci.Name, co.Name AS Country
+FROM city ci
+JOIN country co ON ci.CountryCode = co.Code
+WHERE co.Continent = 'South America'
+  AND ci.Name LIKE '%Serrav%'  -- Replace with actual city prefix
+  AND ci.Name NOT LIKE '%Serravalle';
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
